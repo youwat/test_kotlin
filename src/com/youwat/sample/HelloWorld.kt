9ceba6b7cf84e765126847de4406bb4c60b04e40
superimo <super.imo.you454@gmail.com>
2017-06-01 16:39:54 +0900

    test

--- src/com/youwat/sample/HelloWorld.kt
+++ src/com/youwat/sample/HelloWorld.kt
@@ -1,16 +1,18 @@
-/**
- * Created by superimo on 2017/05/28.
- */
-
 package com.youwat.sample;
 
 fun max(a: Int, b: Int): Int = if(a < b) b else if(a == b) 0 else a
+fun min(a: Int, b: Int): Int = if(a > b) b else if(a == b) 0 else a
 
 fun main(args: Array<String>) {
     val name: String = "Taro"
     val ifRet: Int = if(name == "Taro") 1 else 2
+    fun functions(n: Int) = n + 1
+    val hoge = :: functions
+
 
     println("Hello $name !")
     println("Hello $ifRet !")
     println(max(1,6))
+    println(min(1,6))
+    println(hoge(1))
 }
